aa4db6b068d2c7b53046a818738679240e00720e
danisalarepo2 <151903534+danisalarepo2@users.noreply.github.com>
2023-12-03 21:48:33 +0000

    index client

---
+++ client/index.js
@@ -0,0 +1,22 @@
+// cargando los estilos
+/* eslint-disable no-console */
+import './styles/style.css';
+// mensaje en consola
+/* eslint-disable */
+// Importando estilos de Materialize CSS
+import 'materialize-css/dist/css/materialize.css';
+// Importando scripts de Materialize
+import 'materialize-css/dist/js/materialize';
+/* eslint-enable */
+// Script para borrar book
+import deleteBook from './domains/book.dashboard';
+
+// Inicializando Scripts de Materialize para la interactividad
+M.AutoInit();
+
+// Cargando script en caso de que la URL sea '/book'
+if (window.location.pathname === '/book') {
+  window.deleteBook = deleteBook;
+}
+
+console.log('🎉 Estilos cargados correctamente 🎉');
