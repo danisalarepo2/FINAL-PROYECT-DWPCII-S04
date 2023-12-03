a673b0350912b2a5025a483c11934edaebb7f4d9
danisalarepo2 <151903534+danisalarepo2@users.noreply.github.com>
2023-12-03 21:49:39 +0000

    book router

---
+++ server/domains/books/book.router.js
@@ -0,0 +1,49 @@
+// Importando el Router de Express
+import { Router } from 'express';
+
+// Importando el controlador
+import bookController from './book.controller';
+
+// Importando factory de validación
+import ValidateFactory from '../../services/validateFactory';
+// Importando el validador de proyectos
+import bookValidator from './book.validator';
+
+// Creando una isntancia del enrutador
+const router = new Router();
+
+// Enrutamos
+// GET "/book"
+router.get('/', bookController.showDashboard);
+
+// GET "/book/add"
+router.get('/add', bookController.add);
+
+// POST "/book/add"
+router.post(
+  '/add',
+  ValidateFactory({
+    schema: bookValidator.bookSchema,
+    getObject: bookValidator.getbook,
+  }),
+  bookController.addPost,
+);
+
+// GET "/project/edit/:id"
+router.get('/edit/:id', bookController.edit);
+
+// PUT "/book/edit/:id"
+router.put(
+  '/edit/:id',
+  ValidateFactory({
+    schema: bookValidator.bookSchema,
+    getObject: bookValidator.getbook,
+  }),
+  bookController.editPut,
+);
+
+// DELETE "/book/:id"
+router.delete('/:id', bookController.deleteBook);
+
+// Exporto este tramo de ruta
+export default router;
