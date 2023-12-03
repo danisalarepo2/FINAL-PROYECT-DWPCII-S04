d7fa44658270a8f5d1644d0820f8405a6227834e
danisalarepo2 <151903534+danisalarepo2@users.noreply.github.com>
2023-12-03 22:23:03 +0000

    debbug

---
+++ server/services/debugLogger.js
@@ -0,0 +1,6 @@
+// Importando dependencia debug
+import Debug from 'debug';
+// Establecemos el proceso a debuggear
+const debug = Debug('FINAL-PROYECT-DWPCII-S04');
+// exportando el logger de debuggeo
+export default debug;
