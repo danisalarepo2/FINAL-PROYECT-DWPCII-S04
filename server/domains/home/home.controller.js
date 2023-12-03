cc9af8513b3ca228bb14547c3046d70bf9e1cd12
danisalarepo2 <151903534+danisalarepo2@users.noreply.github.com>
2023-12-03 22:20:55 +0000

    home controller

---
+++ server/domains/home/home.controller.js
@@ -0,0 +1,22 @@
+// Actions methods
+// GET "/"
+// GET "/index"
+const home = (req, res) => {
+  const iconSet = ['⭐', '🤖', '🍉', '📲', '🪸', '🌠', '🦾'];
+  const icon = iconSet[Math.floor(Math.random() * iconSet.length)];
+  res.render('home/homeView', { title: 'Biblos', icon });
+};
+
+// GET "/about"
+const about = (req, res) => {
+  res.render('home/aboutView', {
+    title: 'Biblos | About',
+    appVersion: '1.0.0',
+  });
+};
+
+// Controlador Home
+export default {
+  home,
+  about,
+};
