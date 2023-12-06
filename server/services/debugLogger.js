2d057c998161cf31be4bcf4e18761851616887bb
USERVNEW <152517835+USERVNEW@users.noreply.github.com>
2023-12-06 02:34:03 +0000

    debbug fix

--- server/services/debugLogger.js
+++ server/services/debugLogger.js
@@ -1,6 +1,6 @@
 // Importando dependencia debug
 import Debug from 'debug';
 // Establecemos el proceso a debuggear
-const debug = Debug('FINAL-PROYECT-DWPCII-S04');
+const debug = Debug('FINAL-PROYECT-DWPCII-S04:server');
 // exportando el logger de debuggeo
 export default debug;
