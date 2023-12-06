15c8e74139936c3553bb1ff6fbcf2261814da1d2
USERVNEW <152517835+USERVNEW@users.noreply.github.com>
2023-12-06 02:32:45 +0000

    edit user

--- server/domains/user/user.router.js
+++ server/domains/user/user.router.js
@@ -4,10 +4,18 @@ import { Router } from 'express';
 // Importando el controlador
 import userController from './user.controller';
 
+// Importando el validaacion usuario
+import userValidator from './user.validator';
+
+// Importando el factory de validación
+import ValidateFactory from '../../services/validateFactory';
+
 // Creando una instancia del enrutador
 const router = new Router();
 
 // Enrutamos
+// GET '/user/Dashboard
+router.get(['/', '/dashboard'], userController.showDashboard);
 // GET '/user/login
 router.get('/login', userController.login);
 
@@ -16,6 +24,34 @@ router.get('/logout', userController.logout);
 
 // GET '/user/register
 router.get('/register', userController.register);
+
+// POST '/user/register'
+router.post(
+  '/register',
+  ValidateFactory(userValidator.signUp),
+  userController.registerPost,
+);
+
+// GET "/user/search"
+router.get('/search', userController.search);
+
+// POST "/user/search"
+router.post('/search', userController.resultpost);
+
+// GET "/project/edit/:id"
+router.get('/edit/:id', userController.edit);
+// PUT "/user/edit/:id"
+router.put(
+  '/edit/:id',
+  ValidateFactory({
+    schema: userValidator.signUp.schema,
+    getObject: userValidator.signUp.getObject,
+  }),
+  userController.editPut,
+);
+
+// DELETE "/user/:id"
+router.delete('/:id', userController.deleteUser);
 
 // Exporto este tramo de ruta
 export default router;
